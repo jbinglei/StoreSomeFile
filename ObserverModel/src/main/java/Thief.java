daf212d07d7d9b7dfd49aa86d92b27495f0623c1
jbinglei <jbinglei@126.com>
2018-01-09 22:12:12 +0800

    a

--- ObserverModel/src/main/java/Thief.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 18-1-7.
- */
-public class Thief implements Watcher {
-    @Override
-    public void update() {
-        System.out.println("运输车有行动，强盗准备动手");
-    }
-}
--- ObserverModel/src/main/java/Watcher.java
+++
@@ -1,7 +0,0 @@
-/**
- * 抽象的观察者
- * Created by Administrator on 18-1-7.
- */
-public interface Watcher {
-    public void update();
-} 
